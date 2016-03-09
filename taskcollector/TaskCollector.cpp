e6953c7e68367aca7b4dc48ea12b5888d3d8d5be
banasiakadam60 <banasiakadam60@gmail.com>
2016-03-09 13:53:24 +0100

    Improvement
    1) Added creating an Alpha file
    2) Some improvements
    3) Fix bugs

--- taskcollector/TaskCollector.cpp
+++ taskcollector/TaskCollector.cpp
@@ -1,39 +1,51 @@
 #include "FreeImage.h"
 #include <algorithm>
+#include <iostream>
+#include <memory>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
 #include <string.h>
-#include <list>
-#include <memory>
-#include <queue>
-#include <iostream>
+#include <vector>
 
-using bitmap_t = std::shared_ptr<FIBITMAP>;
+struct Deleter
+{
+	void operator()(FIBITMAP* ptr) const
+	{
+		FreeImage_Unload(ptr);
+	}
+};
+
+using bitmap_ptr = std::unique_ptr<FIBITMAP, Deleter>;
 
 /** Generic image loader
 @param lpszPathName Pointer to the full file name
 @param flag Optional load flag constant
 @return Returns the loaded dib if successful, returns NULL otherwise
 */
-bitmap_t GenericLoader(const char* lpszPathName, int flag) {
-	FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
+bitmap_ptr GenericLoader(const std::string& lpszPathName, int flag) {
+	auto fif = FIF_UNKNOWN;
+
+	// check if file path is not empty
+	if (lpszPathName.empty())
+		return nullptr;
 
 	// check the file signature and deduce its format
 	// (the second argument is currently not used by FreeImage)
-	fif = FreeImage_GetFileType(lpszPathName, 0);
+	fif = FreeImage_GetFileType(lpszPathName.c_str(), 0);
 	if (fif == FIF_UNKNOWN) {
 		// no signature ?
 		// try to guess the file format from the file extension
-		fif = FreeImage_GetFIFFromFilename(lpszPathName);
+		fif = FreeImage_GetFIFFromFilename(lpszPathName.c_str());
 	}
 	// check that the plugin has reading capabilities ...
 	if ((fif != FIF_UNKNOWN) && FreeImage_FIFSupportsReading(fif)) {
 		// ok, let's load the file
-		auto dib = std::make_shared<FIBITMAP>(*FreeImage_Load(fif, lpszPathName, flag));
+		bitmap_ptr dib(FreeImage_Load(fif, lpszPathName.c_str(), flag));
 		// unless a bad file format, we are done !
 		return dib;
 	}
-	return NULL;
+	return nullptr;
 }
 
 /** Generic image writer
@@ -42,17 +54,20 @@ bitmap_t GenericLoader(const char* lpszPathName, int flag) {
 @param flag Optional save flag constant
 @return Returns true if successful, returns false otherwise
 */
-bool GenericWriter(bitmap_t dib, const char* lpszPathName, int flag) {
-	FREE_IMAGE_FORMAT fif = FIF_UNKNOWN;
-	BOOL bSuccess = FALSE;
+bool GenericWriter(const bitmap_ptr& dib, const std::string& lpszPathName, int flag) {
+	auto fif = FIF_UNKNOWN;
+	auto bSuccess = FALSE;
+	// check if file path is not empty
+	if (lpszPathName.empty())
+		return false;
 	if (dib) {
 		// try to guess the file format from the file extension
-		fif = FreeImage_GetFIFFromFilename(lpszPathName);
+		fif = FreeImage_GetFIFFromFilename(lpszPathName.c_str());
 		if (fif != FIF_UNKNOWN) {
 			// check that the plugin has sufficient writing and export capabilities ...
 			if (FreeImage_FIFSupportsWriting(fif) && FreeImage_FIFSupportsExportType(fif, FreeImage_GetImageType(dib.get()))) {
 				// ok, we can save the file
-				bSuccess = FreeImage_Save(fif, dib.get(), lpszPathName, flag);
+				bSuccess = FreeImage_Save(fif, dib.get(), lpszPathName.c_str(), flag);
 				// unless an abnormal bug, we are done !
 			}
 			else {
@@ -84,41 +99,47 @@ void FreeImageErrorHandler(FREE_IMAGE_FORMAT fif, const char *message) {
 class TaskCollector {
 
 protected:
-	std::vector<bitmap_t> chunks;
-	std::vector<bitmap_t> alphaChunks;
+	std::vector<bitmap_ptr> chunks;
+	std::vector<bitmap_ptr> alphaChunks;
 
 public:
 	TaskCollector() = default;
-	TaskCollector(const TaskCollector&) = default;
-	//TaskCollector(TaskCollector &&) = default;	// for some reason visual think it's incorrect constructor
-	~TaskCollector()
+	TaskCollector(const TaskCollector&) = delete;
+	TaskCollector(TaskCollector &&other)
 	{
-		std::for_each(chunks.begin(), chunks.end(), [](bitmap_t el){ FreeImage_Unload(el.get()); });
-		std::for_each(alphaChunks.begin(), alphaChunks.end(), [](bitmap_t el){ FreeImage_Unload(el.get()); });
+		chunks = std::move(other.chunks);
+		alphaChunks = std::move(other.alphaChunks);
 	}
+	~TaskCollector() = default;
 
-	bool addImgFile(const char* pathName, int flag = 0)  {
-		auto img = GenericLoader(pathName, flag);
-		if (img == NULL)
+	bool addImgFile(const std::string& pathName, int flag = 0)  {
+		if (pathName.empty())
 			return false;
-		chunks.push_back(img);
+		auto img = GenericLoader(pathName.c_str(), flag);
+		if (!img)
+			return false;
+		chunks.emplace_back(std::move(img));
 		return true;
 	};
 
-	bool addAlphaFile(const char* pathName, int flag = 0) {
-		auto img = GenericLoader(pathName, flag);
-		if (img == NULL)
+	bool addAlphaFile(const std::string& pathName, int flag = 0) {
+		if (pathName.empty())
 			return false;
-		alphaChunks.push_back(img);
+		auto img = GenericLoader(pathName.c_str(), flag);
+		if (!img)
+			return false;
+		alphaChunks.emplace_back(std::move(img));
 		return true;
 	};
 
-	virtual bitmap_t finalize(bool showProgress = false) = 0;
+	virtual bitmap_ptr finalize(bool showProgress = false) = 0;
 
-	bool finalizeAndSave(const char* outputPath) {
+	bool finalizeAndSave(const std::string& outputPath) {
+		if (outputPath.empty())
+			return false;
 		std::cout << "finalize & safe " << outputPath << std::endl;
 		auto img = finalize();
-		return 	GenericWriter(img, outputPath, EXR_FLOAT);
+		return GenericWriter(img, outputPath, EXR_FLOAT);
 	};
 
 };
@@ -126,57 +147,55 @@ public:
 class AddTaskCollector : public TaskCollector {
 
 public:
-	bitmap_t finalize(bool showProgress = false) {
+	bitmap_ptr finalize(bool showProgress = false) {
 		if (chunks.empty()) {
-			return NULL;
+			return nullptr;
 		}
 		if (showProgress) {
 			printf("Adding all accepted chunks to the final image\n");
 		}
 
-		auto it = chunks.begin();
-		unsigned int width = FreeImage_GetWidth(it->get());
-		unsigned int height = FreeImage_GetHeight(it->get());
-		FREE_IMAGE_TYPE type = FreeImage_GetImageType(it->get());
+		const auto it = chunks.begin();
+		const auto width = FreeImage_GetWidth(it->get());
+		const auto height = FreeImage_GetHeight(it->get());
+		const auto type = FreeImage_GetImageType(it->get());
 
-		auto finalImage = std::make_shared<FIBITMAP>(*FreeImage_Copy(it->get(), 0, height, width, 0));
+		bitmap_ptr finalImage(FreeImage_Copy(it->get(), 0, height, width, 0));
 
-		auto chunksWorker = [type, height, width, finalImage](bitmap_t el)
+		auto RGBChunkWorker = [=, &finalImage](const bitmap_ptr& el)
 		{
-			if (type == FIT_RGBF)
-			{
-				for (unsigned int y = 0; y < height; ++y) {
-					FIRGBF *srcbits = (FIRGBF *)FreeImage_GetScanLine(el.get(), y);
-					FIRGBF *dstbits = (FIRGBF *)FreeImage_GetScanLine(finalImage.get(), y);
+			for (unsigned int y = 0; y < height; ++y) {
+				auto srcbits = reinterpret_cast<FIRGBF *>(FreeImage_GetScanLine(el.get(), y));
+				auto dstbits = reinterpret_cast<FIRGBF *>(FreeImage_GetScanLine(finalImage.get(), y));
 
-					for (unsigned int x = 0; x < width; ++x) {
-						dstbits[x].red += srcbits[x].red;
-						dstbits[x].blue += srcbits[x].blue;
-						dstbits[x].green += srcbits[x].green;
-					}
-				}
-			}
-			else if (type == FIT_RGBAF)
-			{
-				for (unsigned int y = 0; y < height; ++y) {
-					FIRGBAF *srcbits = (FIRGBAF *)FreeImage_GetScanLine(el.get(), y);
-					FIRGBAF *dstbits = (FIRGBAF *)FreeImage_GetScanLine(finalImage.get(), y);
-
-					for (unsigned int x = 0; x < width; ++x) {
-						dstbits[x].red += srcbits[x].red;
-						dstbits[x].blue += srcbits[x].blue;
-						dstbits[x].green += srcbits[x].green;
-						dstbits[x].alpha += srcbits[x].alpha;
-					}
+				for (unsigned int x = 0; x < width; ++x) {
+					dstbits[x].red += srcbits[x].red;
+					dstbits[x].blue += srcbits[x].blue;
+					dstbits[x].green += srcbits[x].green;
 				}
 			}
 		};
 
-		auto alphaChunksWorker = [height, width, finalImage](bitmap_t el)
+		auto RGBAChunkWorker = [=, &finalImage](const bitmap_ptr& el)
 		{
 			for (unsigned int y = 0; y < height; ++y) {
-				FIRGBAF *srcbits = (FIRGBAF *)FreeImage_GetScanLine(el.get(), y);
-				FIRGBAF *dstbits = (FIRGBAF *)FreeImage_GetScanLine(finalImage.get(), y);
+				const auto srcbits = reinterpret_cast<FIRGBAF *>(FreeImage_GetScanLine(el.get(), y));
+				auto dstbits = reinterpret_cast<FIRGBAF *>(FreeImage_GetScanLine(finalImage.get(), y));
+
+				for (unsigned int x = 0; x < width; ++x) {
+					dstbits[x].red += srcbits[x].red;
+					dstbits[x].blue += srcbits[x].blue;
+					dstbits[x].green += srcbits[x].green;
+					dstbits[x].alpha += srcbits[x].alpha;
+				}
+			}
+		};
+
+		auto alphaChunksWorker = [height, width, &finalImage](bitmap_ptr& el)
+		{
+			for (unsigned int y = 0; y < height; ++y) {
+				const auto srcbits = (FIRGBAF *)FreeImage_GetScanLine(el.get(), y);
+				auto dstbits = (FIRGBAF *)FreeImage_GetScanLine(finalImage.get(), y);
 
 				for (unsigned int x = 0; x < width; ++x) {
 					dstbits[x].alpha += srcbits[x].red + srcbits[x].blue + srcbits[x].green;
@@ -184,7 +203,10 @@ public:
 			}
 		};
 
-		std::for_each(++chunks.begin(), chunks.end(), chunksWorker);
+		if (type == FIT_RGBF)
+			std::for_each(std::next(chunks.begin()), chunks.end(), RGBChunkWorker);
+		else if (type == FIT_RGBAF)
+			std::for_each(std::next(chunks.begin()), chunks.end(), RGBAChunkWorker);
 		std::for_each(alphaChunks.begin(), alphaChunks.end(), alphaChunksWorker);
 
 		return finalImage;
@@ -194,58 +216,59 @@ public:
 class PasteTaskCollector : public TaskCollector {
 
 public:
-	bitmap_t finalize(bool showProgress = true) {
+	bitmap_ptr finalize(bool showProgress = true) {
 		if (chunks.empty()) {
-			return NULL;
+			return nullptr;
 		}
 		if (showProgress) {
 			printf("Adding all accepted chunks to the final image\n");
 		}
-		auto it = chunks.begin();
-		unsigned int width = FreeImage_GetWidth(it->get());
-		unsigned int chunkHeight = FreeImage_GetHeight(it->get());
-		unsigned int height = chunkHeight * chunks.size();
-		unsigned int currentHeight = height - chunkHeight;
+		const auto it = chunks.begin();
+		const auto width = FreeImage_GetWidth(it->get());
+		const auto chunkHeight = FreeImage_GetHeight(it->get());
+		const auto height = chunkHeight * chunks.size();
+		auto currentHeight = height - chunkHeight;
 
-		FREE_IMAGE_TYPE type = FreeImage_GetImageType(it->get());
-		int bpp = FreeImage_GetBPP(it->get());
-		auto finalImage = std::make_shared<FIBITMAP>(*FreeImage_AllocateT(type, width, height, bpp));
+		const auto type = FreeImage_GetImageType(it->get());
+		const auto bpp = FreeImage_GetBPP(it->get());
+		bitmap_ptr finalImage(FreeImage_AllocateT(type, width, height, bpp));
 
-		auto chunkWorker = [type, chunkHeight, finalImage, &currentHeight, width](bitmap_t el)
+		auto RGBChunkWorker = [=, &finalImage, &currentHeight](const bitmap_ptr& el)
 		{
-			if (type == FIT_RGBF)
-			{
-				for (unsigned int y = 0; y < chunkHeight; ++y) {
-					FIRGBF *srcbits = (FIRGBF *)FreeImage_GetScanLine(el.get(), y);
-					FIRGBF *dstbits = (FIRGBF *)FreeImage_GetScanLine(finalImage.get(), y + currentHeight);
-					for (unsigned int x = 0; x < width; ++x) {
-						dstbits[x].red = srcbits[x].red;
-						dstbits[x].blue = srcbits[x].blue;
-						dstbits[x].green = srcbits[x].green;
-					}
-				}
-			}
-			else if (type == FIT_RGBAF)
-			{
-				for (unsigned int y = 0; y < chunkHeight; ++y) {
-					FIRGBAF *srcbits = (FIRGBAF *)FreeImage_GetScanLine(el.get(), y);
-					FIRGBAF *dstbits = (FIRGBAF *)FreeImage_GetScanLine(finalImage.get(), y + currentHeight);
-					for (unsigned int x = 0; x < width; ++x) {
-						dstbits[x].red = srcbits[x].red;
-						dstbits[x].blue = srcbits[x].blue;
-						dstbits[x].green = srcbits[x].green;
-						dstbits[x].alpha = srcbits[x].alpha;
-					}
+			for (unsigned int y = 0; y < chunkHeight; ++y) {
+				const auto srcbits = reinterpret_cast<FIRGBF *>(FreeImage_GetScanLine(el.get(), y));
+				auto dstbits = reinterpret_cast<FIRGBF *>(FreeImage_GetScanLine(finalImage.get(), y + currentHeight));
+				for (unsigned int x = 0; x < width; ++x) {
+					dstbits[x].red = srcbits[x].red;
+					dstbits[x].blue = srcbits[x].blue;
+					dstbits[x].green = srcbits[x].green;
 				}
 			}
 			currentHeight -= chunkHeight;
 		};
 
-		std::for_each(chunks.begin(), chunks.end(), chunkWorker);
+		auto RGBAChunkWorker = [=, &finalImage, &currentHeight](const bitmap_ptr& el)
+		{
+			for (unsigned int y = 0; y < chunkHeight; ++y) {
+				const auto srcbits = reinterpret_cast<FIRGBAF *>(FreeImage_GetScanLine(el.get(), y));
+				auto dstbits = reinterpret_cast<FIRGBAF *>(FreeImage_GetScanLine(finalImage.get(), y + currentHeight));
+				for (unsigned int x = 0; x < width; ++x) {
+					dstbits[x].red = srcbits[x].red;
+					dstbits[x].blue = srcbits[x].blue;
+					dstbits[x].green = srcbits[x].green;
+					dstbits[x].alpha = srcbits[x].alpha;
+				}
+			}
+			currentHeight -= chunkHeight;
+		};
+
+		if (type == FIT_RGBF)
+			std::for_each(chunks.begin(), chunks.end(), RGBChunkWorker);
+		else if (type == FIT_RGBAF)
+			std::for_each(chunks.begin(), chunks.end(), RGBAChunkWorker);
 
 		return finalImage;
 	}
-
 };
 
 int
@@ -277,8 +300,8 @@ main(int argc, char *argv[]) {
 		alphaTaskCollector = std::make_unique<AddTaskCollector>();
 	}
 	else if (strcmp(argv[1], "paste") == 0) {
-		taskCollector = std::make_unique<AddTaskCollector>();
-		alphaTaskCollector = std::make_unique<AddTaskCollector>();
+		taskCollector = std::make_unique<PasteTaskCollector>();
+		alphaTaskCollector = std::make_unique<PasteTaskCollector>();
 	}
 	else {
 		printf("Possible types: 'add', 'paste'\n");
@@ -303,11 +326,10 @@ main(int argc, char *argv[]) {
 
 	std::string name(argv[2]);
 	auto it = name.find_last_of('.');
-	name = it == std::string::npos ? name + ".exr" : name.substr(0, it) + ".Alpha.exr";
+	name = ( (it == std::string::npos) ? (name + ".exr") : (name.substr(0, it) + ".Alpha.exr") );
 
 	taskCollector->finalizeAndSave(argv[2]);
-	alphaTaskCollector->finalizeAndSave(name.c_str());
-
+	alphaTaskCollector->finalizeAndSave(name);
 	// call this ONLY when linking with FreeImage as a static library
 #ifdef FREEIMAGE_LIB
 	FreeImage_DeInitialise();
